f1a6142961d83fe04ca0c0cf99c326e2e2a5e1e8
qian.y1 <qian.y1@stu.janesville.k12.wi.us>
2022-12-22 08:34:09 -0600

    update

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        double n1 = 2.7;
+        double n2 = (double)8.1/3;
+        System.out.println(n1);
+        System.out.println(n2);
+        if (n1 == n2) System.out.println("相等");
+    }
+}
