750e9518722c0d83e7c36da4ae31ef02194fc3ea
qian.y1 <qian.y1@stu.janesville.k12.wi.us>
2022-12-24 09:43:15 -0600

    update

--- src/Switch01.java
+++ src/Switch01.java
@@ -1,12 +1,12 @@
-//接收一个字符，一个字符表示一个星期中的一天，输出星期几
 import java.util.Scanner;
 public class Switch01 {
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
 
-        System.out.println("请输入一个字符：");
-        char ch = kbd.next().charAt(0);
-        switch (ch) {
+        //接收一个字符，一个字符表示一个星期中的一天，输出星期几
+        System.out.print("请输入一个字符：");
+        char c1 = kbd.next().charAt(0);
+        switch (c1) {
             case 'a' :
                 System.out.println("今天是星期一");
                 break;
@@ -19,6 +19,36 @@ public class Switch01 {
             //......
             default:
                 System.out.println("你输入的字符不正确");
+        }
+
+        //小写转大写
+        System.out.print("输入一个字符： ");
+        char c2 = kbd.next().charAt(0);
+        switch (c2) {
+            case 'a' :
+                System.out.println("A");
+                break;
+            case 'b' :
+                System.out.println("B");
+                break;
+            default :
+                System.out.println("Other");
+        }
+
+        //判断学生成绩是否合格
+        System.out.print("输入成绩：");
+        int score = kbd.nextInt();
+        switch (score) {
+            case score >= 60 :
+
+        }
+
+        //根据指定月份打印季节
+        System.out.print("输入月份：");
+        int month = kbd.nextInt();
+        switch (month) {
+            case 1 :
+
         }
     }
 }
